528c256088f3c537dc31d0f4755bed57f5a2dd47
Enrico Ros <enrico.ros@gmail.com>
2016-02-11 20:51:59 -0800

    Vast improvements; looks almost good!

--- polyfills.js
+++ polyfills.js
@@ -7,3 +7,8 @@ if (!Array.prototype.randomElement) {
         return this[Math.floor(Math.random() * this.length)]
     };
 }
+
+// we love to be nice to the eye
+$.easing.easeOutCubic = function (x, t, b, c, d) {
+    return c*((t=t/d-1)*t*t + 1) + b;
+};
